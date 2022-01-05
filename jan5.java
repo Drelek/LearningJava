63e1573cec7a21a6825b649ffe8fa9aa3ba2c5af
Jared Burkamper <jaredburkamper@gmail.com>
2022-01-05 15:05:08 -0600

    Added rectangle

--- jan5.java
+++ jan5.java
@@ -93,3 +93,42 @@ class Calculator {
     return sum;
   }
 }
+
+class Rectangle {
+  private double length;
+  private double width;
+
+  public Rectangle() {
+    length = 1;
+    width = 1;
+  }
+
+  public Rectangle(double length, double width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  public double area() {
+    return length * width;
+  }
+
+  public double perimeter() {
+    return length + width + length + width;
+  }
+
+  public double getLength() {
+    return length;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setLength(double length) {
+    this.length = length;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+}
